4f8a6854dcb5a50f1e03b7a928cdc9a5f14a5e12
Nazhifa Naura Yasmin <nazhifa.naura.yasmin24044@mhs.uingusdur.ac.id>
2026-09-14 09:06:42 +0700

    feat: tambahan untuk LaporanUdara

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
+import { useState, useEffect } from "react";
+import WeatherCard from "./components/WeatherCard";
+import SearchBox from "./components/SearchBox";
+import RiwayatList from "./components/RiwayatList";
 
-export default function App() {
+export default function HalamanUtama() {
+  const [kotaAktif, setKotaAktif] = useState("Pekalongan");
+  const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
+  
+  useEffect(() => {
+    console.log("Kota aktif berubah menjadi:", kotaAktif);
+  }, [kotaAktif]);
+
+  function handleCari (kota: string) {
+    setKotaAktif(kota);
+    if (!riwayat.includes(kota)) {
+      setRiwayat([...riwayat, kota]);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 16, gap: 16}}>
+      <SearchBox onCari={handleCari} />
+      <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
+      <RiwayatList daftarKota={riwayat} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
